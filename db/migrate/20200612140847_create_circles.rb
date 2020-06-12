f3cf658fa8927157a9a76b97c4c20c370f33ef67
shiroemons <shiroemons@gmail.com>
2020-06-12 23:11:06 +0900

    Add circle model

---
+++ db/migrate/20200612140847_create_circles.rb
@@ -0,0 +1,9 @@
+class CreateCircles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :circles, id: :uuid do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_162807) do
+ActiveRecord::Schema.define(version: 2020_06_12_140847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "circles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "karaoke_delivery_models", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
