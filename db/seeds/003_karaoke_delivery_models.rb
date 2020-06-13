63947e1d4827788147bc65454e1050d0d0532e54
shiroemons <shiroemons@gmail.com>
2020-06-13 23:01:07 +0900

    Update karaoke_delivery_models seed

--- db/seeds/003_karaoke_delivery_models.rb
+++ db/seeds/003_karaoke_delivery_models.rb
@@ -1,17 +1,19 @@
 require 'csv'
 
-ActiveRecord::Base.connection.execute("TRUNCATE TABLE karaoke_delivery_models;")
 insert_data = []
 now = Time.now
 order = 0
 karaoke_delivery_models = CSV.table('db/fixtures/karaoke_delivery_models.tsv', col_sep: "\t", converters: nil).each do |kdm|
   order += 1
-  insert_data << { 
-    name: kdm[:name],
-    karaoke_type: kdm[:karaoke_type],
-    order: order,
-    created_at: now,
-    updated_at: now
-  }
+  exist = KaraokeDeliveryModel.exists?(name: kdm[:name], karaoke_type: kdm[:karaoke_type])
+  unless exist
+    insert_data << {
+      name: kdm[:name],
+      karaoke_type: kdm[:karaoke_type],
+      order: order,
+      created_at: now,
+      updated_at: now
+    }
+  end
 end
-KaraokeDeliveryModel.insert_all(insert_data)
+KaraokeDeliveryModel.insert_all(insert_data) if insert_data.present?
