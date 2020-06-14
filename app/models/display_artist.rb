c0a4d715ee82d2faa44a0c8e03a7d36faf52f955
shiroemons <shiroemons@gmail.com>
2020-06-14 17:41:48 +0900

    Add fetch_joysound_music_post_artist

--- app/models/display_artist.rb
+++ app/models/display_artist.rb
@@ -1,7 +1,7 @@
 class DisplayArtist < ApplicationRecord
   has_many :display_artists_circles
   has_many :circles, through: :display_artists_circles
-  has_many :songs
+  has_many :songs, dependent: :destroy
 
   scope :joysound, -> { where(karaoke_type: "JOYSOUND") }
   scope :name_reading_empty, -> { where(name_reading: "") }
@@ -24,4 +24,55 @@ class DisplayArtist < ApplicationRecord
     end
   end
 
+  def self.fetch_joysound_music_post_artist
+    base_url = "https://www.joysound.com/web/search/artist?match=1&keyword="
+    browser = Ferrum::Browser.new(timeout: 30, window_size: [1440, 2000])
+
+    artists = JoysoundMusicPost.pluck(:artist).uniq.sort
+    error_artist = []
+
+    artists.each do |artist|
+      rescue_count = 0
+      begin
+        url = base_url + CGI.escape(artist)
+        browser.goto(url)
+
+        result_list_selector = "#searchresult > ul > li"
+        browser.css(result_list_selector).each do |el|
+          no_data = el.inner_text
+          if no_data == "該当データがありません"
+            JoysoundMusicPost.where(artist: artist)&.destroy_all
+            DisplayArtist.find_by(name: artist, karaoke_type: "JOYSOUND(うたスキ)")&.destory
+          else
+            next if DisplayArtist.exists?(name: artist, karaoke_type: "JOYSOUND(うたスキ)")
+
+            option = el.at_css("div > div > div.jp-cmp-list-inline-003").inner_text
+            option.gsub!("ウィキペディア", "")
+            if option.blank?
+              display_artist = el.at_css("h3.jp-cmp-music-title-001").inner_text
+              display_artist.gsub!(" 新曲あり", "")
+              if artist == display_artist
+                el.at_css("a").focus.click
+                artist_selector = "#jp-cmp-main > section:nth-child(2) > header > div.jp-cmp-h1-003-title > h1 > span"
+                artist_el = browser.at_css(artist_selector)
+                name_reading = artist_el.inner_text.gsub(/[（）]/, "")
+
+                DisplayArtist.find_or_create_by!(name: display_artist, name_reading: name_reading, karaoke_type: "JOYSOUND(うたスキ)", url: browser.current_url)
+              end
+            end
+          end
+        end
+      rescue Ferrum::NodeNotFoundError => e
+        logger.debug(e)
+        rescue_count += 1
+        if rescue_count > 3
+          browser.screenshot(path: "tmp/music_post_#{artist.gsub("/", "／")}.png")
+          error_artist << artist
+        else
+          retry
+        end
+      end
+    end
+    logger.debug("未登録アーティスト：" + error_artist) if error_artist.present?
+  end
 end
--- test/models/display_artist_test.rb
+++ test/models/display_artist_test.rb
@@ -4,4 +4,8 @@ class DisplayArtistTest < ActiveSupport::TestCase
   # test ".fetch_joysound_artist" do
   #   DisplayArtist.fetch_joysound_artist
   # end
+
+  # test ".fetch_joysound_music_post_artist" do
+  #   DisplayArtist.fetch_joysound_music_post_artist
+  # end
 end
