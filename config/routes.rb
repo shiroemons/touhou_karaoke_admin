89c116cd47e286e6b7cad5feb808424ba726bffa
shiroemons <shiroemons@gmail.com>
2020-06-30 23:12:00 +0900

    Change the order of the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,20 @@ Rails.application.routes.draw do
   root to: "admin/songs#index"
   namespace :admin do
       resources :songs
-      resources :song_with_joysound_utasukis
-      resources :joysound_music_posts
-      resources :display_artists
       resources :originals
+      resources :original_songs
+      resources :karaoke_delivery_models
+      resources :circles
+      resources :display_artists
+      resources :dam_songs
+      resources :joysound_songs
+      resources :joysound_music_posts
+      resources :song_with_joysound_utasukis
+      resources :song_with_dam_ouchikaraokes
       resources :dam_artist_urls
       resources :display_artists_circles
       resources :songs_original_songs
-      resources :dam_songs
       resources :songs_karaoke_delivery_models
-      resources :song_with_dam_ouchikaraokes
-      resources :joysound_songs
-      resources :circles
-      resources :karaoke_delivery_models
-      resources :original_songs
 
       root to: "songs#index"
     end
