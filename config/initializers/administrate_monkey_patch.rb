fd8c65b968ffad35fdd8d66382e9cd463f4440a0
shiroemons <shiroemons@gmail.com>
2020-07-01 20:38:44 +0900

    Add administrate monkey patch

---
+++ config/initializers/administrate_monkey_patch.rb
@@ -0,0 +1,8 @@
+Administrate::Order.class_eval do
+  def order_by_count(relation)
+    relation.
+        left_joins(attribute.to_sym).
+        group(relation.primary_key.to_sym).
+        reorder("COUNT(#{attribute}.#{reflect_association(relation).association_primary_key}) #{direction}")
+  end
+end
