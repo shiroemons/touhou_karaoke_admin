c6fdd888eff4ba932b3d86180b96042686f32439
shiroemons <shiroemons@gmail.com>
2022-05-06 23:53:30 +0900

    :cop: rubocop --auto-correct --only Style/CommentAnnotation

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -24,7 +24,7 @@ module Admin
     ].freeze
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      # TODO: Add authentication logic here.
     end
 
     # Override this value to specify the number of elements to display at a time
