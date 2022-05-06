3f5a6964560139569039d8971dd12c2aad7af1fa
shiroemons <shiroemons@gmail.com>
2022-05-06 23:54:49 +0900

    :cop: rubocop --auto-correct --only Style/Next

--- db/seeds/003_karaoke_delivery_models.rb
+++ db/seeds/003_karaoke_delivery_models.rb
@@ -6,14 +6,13 @@ order = 0
 karaoke_delivery_models = CSV.table('db/fixtures/karaoke_delivery_models.tsv', col_sep: "\t", converters: nil).each do |kdm|
   order += 1
   exist = KaraokeDeliveryModel.exists?(name: kdm[:name], karaoke_type: kdm[:karaoke_type])
-  unless exist
-    insert_data << {
-      name: kdm[:name],
-      karaoke_type: kdm[:karaoke_type],
-      order:,
-      created_at: now,
-      updated_at: now
-    }
-  end
+  next if exist
+  insert_data << {
+    name: kdm[:name],
+    karaoke_type: kdm[:karaoke_type],
+    order:,
+    created_at: now,
+    updated_at: now
+  }
 end
 KaraokeDeliveryModel.insert_all(insert_data) if insert_data.present?
