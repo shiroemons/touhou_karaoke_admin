e25a4e036670220237ebcfec2590bf0d8040bbcc
shiroemons <shiroemons@gmail.com>
2022-05-08 00:05:11 +0900

    Update export_karaoke_songs

--- lib/export_karaoke_songs.rb
+++ lib/export_karaoke_songs.rb
@@ -1,5 +1,5 @@
 File.open('tmp/karaoke_songs.tsv', 'w') do |f|
-  f.puts "karaoke_type\tsong_number\ttitle\turl\toriginal_songs\tyoutube_url\tnicovideo_url\tapple_music_url\tyoutube_music_url"
+  f.puts "karaoke_type\tsong_number\ttitle\turl\toriginal_songs\tyoutube_url\tnicovideo_url\tapple_music_url"
   Song.includes(:original_songs).order(:karaoke_type).each do |song|
     karaoke_type = song.karaoke_type
     song_number = song.song_number
@@ -9,8 +9,8 @@ File.open('tmp/karaoke_songs.tsv', 'w') do |f|
     youtube_url = song.youtube_url
     nicovideo_url = song.nicovideo_url
     apple_music_url = song.apple_music_url
-    youtube_music_url = song.youtube_music_url
+    # next if original_songs.present?
 
-    f.puts "#{karaoke_type}\t#{song_number}\t#{title}\t#{url}\t#{original_songs}\t#{youtube_url}\t#{nicovideo_url}\t#{apple_music_url}\t#{youtube_music_url}"
+    f.puts "#{karaoke_type}\t#{song_number}\t#{title}\t#{url}\t#{original_songs}\t#{youtube_url}\t#{nicovideo_url}\t#{apple_music_url}"
   end
 end
