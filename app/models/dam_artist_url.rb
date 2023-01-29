6d53ddb77178f86faa606a58bc5503bd80430c5f
shiroemons <shiroemons@gmail.com>
2023-01-29 19:43:34 +0900

    Ferrum::Browser.newの位置を変更

--- app/models/dam_artist_url.rb
+++ app/models/dam_artist_url.rb
@@ -2,17 +2,17 @@ class DamArtistUrl < ApplicationRecord
   validates :url, presence: true
 
   def self.fetch_dam_artist
-    @browser = Ferrum::Browser.new(timeout: 30, window_size: [1440, 900], browser_options: { 'no-sandbox': nil })
     DamArtistUrl.all.find_each do |dau|
-      next if DisplayArtist.exists?(karaoke_type: "DAM", url: dau.url)
+      next unless DisplayArtist.exists?(karaoke_type: "DAM", url: dau.url, name_reading: "")
 
       dam_artist_page_parser(dau.url)
     end
-    @browser.quit
   end
 
   def self.dam_artist_page_parser(url)
     retry_count = 0
+    @browser = Ferrum::Browser.new(timeout: 10, window_size: [1440, 900], browser_options: { 'no-sandbox': nil })
+
     begin
       @browser.goto(url)
       @browser.network.wait_for_idle(duration: 1.0)
@@ -26,9 +26,11 @@ class DamArtistUrl < ApplicationRecord
         record.name_reading = name_reading
         record.save! if record.changed?
       end
-    rescue Ferrum::TimeoutError => e
+      @browser.quit
+    rescue => e
       logger.error(e)
-      @browser.network.clear(:traffic)
+      @browser.quit
+      @browser = Ferrum::Browser.new(timeout: 10, window_size: [1440, 900], browser_options: { 'no-sandbox': nil })
       retry_count += 1
       retry unless retry_count > 3
     end
