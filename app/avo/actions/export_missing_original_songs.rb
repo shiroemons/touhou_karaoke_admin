da5832726f5658bb49937d0ed3e74864b2ef7bb0
shiroemons <shiroemons@gmail.com>
2024-09-16 14:55:19 +0900

    空白行を追加

--- app/avo/actions/export_missing_original_songs.rb
+++ app/avo/actions/export_missing_original_songs.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'csv'
 
 class ExportMissingOriginalSongs < Avo::BaseAction
--- app/avo/actions/export_songs.rb
+++ app/avo/actions/export_songs.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'csv'
 
 class ExportSongs < Avo::BaseAction
