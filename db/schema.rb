265dca3403b19df078539abccc806cf1b7470ce8
shiroemons <shiroemons@gmail.com>
2026-01-24 16:36:17 +0900

    DBスキーマとシードファイルを更新
    - Rails 8.0形式にスキーマを更新
    - シードファイルのファイルパス表示を簡潔化

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_15_030553) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_030553) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
 
   create_table "circles", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Rails.root.glob('db/seeds/*.rb').each do |file|
-  puts "Processing #{file.split('/').last}"
+  puts "Processing #{file.basename}"
   require file
 end
