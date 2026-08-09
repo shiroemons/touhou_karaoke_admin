82a81bf1a3896a289f08c6bacc6622ff3a500a1c
shiroemons <shiroemons@gmail.com>
2026-08-09 16:27:12 +0900

    fix: フォームエラーから入力欄へ移動可能にする

--- test/controllers/admin/resources_controller_test.rb
+++ test/controllers/admin/resources_controller_test.rb
@@ -1037,7 +1037,8 @@ module Admin
       end
 
       assert_response :unprocessable_content
-      assert_select '.admin-errors'
+      assert_select '.admin-errors[role="alert"][aria-live="assertive"]'
+      assert_select '.admin-errors a[href="#karaoke_delivery_model_name"]', text: /機種名/
       assert_select '.admin-form-row-invalid input[name="karaoke_delivery_model[name]"][aria-invalid="true"][aria-describedby="karaoke_delivery_model_name_error"]'
       assert_select '#karaoke_delivery_model_name_error.admin-field-errors', text: /機種名/
     end
